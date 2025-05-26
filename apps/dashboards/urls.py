9a8a9538f8f3103bad90b2aa74799d1f0657cc24
ZuLu29 <markneilconde850@gmail.com>
2025-05-26 17:24:03 +0800

    tapos na ya

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from .views import DashboardsView
-
-
+from . import views
 
 urlpatterns = [
+    # Main dashboard page
     path(
         "",
         DashboardsView.as_view(template_name="dashboard_analytics.html"),
         name="index",
-        
-    )
-    
+    ),
+
+    # API endpoints for charts
+    path('api/movies-over-time/', views.movies_over_time, name='movies_over_time'),
+    path('api/genre-distribution/', views.genre_distribution, name='genre_distribution'),
+    path('api/character-movie-counts/', views.character_movie_counts, name='character_movie_counts'),
+    path('api/most-featured-strawhats/', views.most_featured_strawhats, name='most_featured_strawhats'),
 ]
