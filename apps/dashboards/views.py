0875b3fc33d0e442fde005c5ad1630c67ecf88c9
ZuLu29 <markneilconde850@gmail.com>
2025-05-26 17:09:05 +0800

    test2

--- apps/dashboards/views.py
+++ apps/dashboards/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 from web_project import TemplateLayout
+from django.http import JsonResponse
+from django.db.models import Count
+from django.db.models.functions import ExtractYear
 
+from .models import Movie, Genre, Character
 
 """
 This file is a view controller for multiple pages as a module.
@@ -16,3 +20,55 @@ class DashboardsView(TemplateView):
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
 
         return context
+
+def movies_over_time(request):
+    data = (
+        Movie.objects
+        .annotate(year=ExtractYear('release_date'))
+        .values('year')
+        .annotate(count=Count('id'))
+        .order_by('year')
+    )
+    return JsonResponse({
+        'years': [entry['year'] for entry in data],
+        'counts': [entry['count'] for entry in data]
+    })
+
+def genre_distribution(request):
+    data = (
+        Movie.objects
+        .values('genre__name')
+        .annotate(count=Count('id'))
+        .order_by('genre__name')
+    )
+    return JsonResponse({
+        'labels': [entry['genre__name'] for entry in data],
+        'counts': [entry['count'] for entry in data]
+    })
+
+def character_movie_counts(request):
+    data = (
+        Character.objects
+        .annotate(movie_count=Count('movies'))
+        .filter(movie_count__gt=0)
+        .values('name', 'movie_count')
+        .order_by('-movie_count')
+    )
+    return JsonResponse({
+        'characters': [entry['name'] for entry in data],
+        'counts': [entry['movie_count'] for entry in data]
+    })
+def most_featured_strawhats(request):
+    strawhats = ['Monkey D. Luffy', 'Roronoa Zoro', 'Nami', 'Sanji', 'Usopp', 'Chopper', 'Robin', 'Franky', 'Brook', 'Jinbe']
+
+    data = (
+        Character.objects
+        .filter(name__in=strawhats)
+        .annotate(movie_count=Count('movies'))
+        .values('name', 'movie_count')
+        .order_by('-movie_count')
+    )
+    return JsonResponse({
+        'labels': [entry['name'] for entry in data],
+        'counts': [entry['movie_count'] for entry in data]
+    })
--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
 
     # Dashboard urls
     path("", include("apps.dashboards.urls")),
+    
 ]
+
 
 handler404 = SystemView.as_view(template_name="pages_misc_error.html", status=404)
 handler400 = SystemView.as_view(template_name="pages_misc_error.html", status=400)
